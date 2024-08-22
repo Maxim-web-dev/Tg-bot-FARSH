0406917b3f87398cf8abd009cdf2bfb3f3e5c4a6
Maxim Khanukaev <Andreymaxim387@gmail.com>
2024-08-22 13:06:46 +0300

    Создал utils, tg hook, импортировал зависимости

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from 'react'
+
+import { useTelegram } from './hooks/useTelegram'
+import { useCartStore } from './store/cartStore'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { tg } = useTelegram()
+  const { isOpen } = useCartStore()
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  useEffect(() => {
+    tg.ready()
+    tg.expand()
+  }, [])
+
+  // if (isOpen) return <Cart />
+
+  // return <Menu />
 }
 
 export default App
