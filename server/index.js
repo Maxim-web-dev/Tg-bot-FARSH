03bdcc750e7905141e72857d3dcf0fdc82dd1529
Maxim Khanukaev <Andreymaxim387@gmail.com>
2024-08-25 11:41:35 +0300

    Установил dotenv, исправил функцию отправки заказа пользователю

--- server/index.js
+++ server/index.js
@@ -1,37 +1,44 @@
-import TelegramBot from "node-telegram-bot-api";
 import cors from "cors";
 import express from "express";
+import TelegramBot from "node-telegram-bot-api";
+import 'dotenv/config'
+// import pkg from 'pg';
+
 import { parseData } from "./utils/parseData.js";
 
-const token = "7547383848:AAHElVLpE49oL5rH1Ez3hDSdK-bxkqZwLqo";
-const webAppUrl = 'https://tg-rest-bot.vercel.app'
-// const webAppUrl = "https://fc3d-198-244-233-81.ngrok-free.app";
-const bot = new TelegramBot(token, { polling: true });
+// const { Client } = pkg;
+
+const bot = new TelegramBot(process.env.TOKEN, { polling: true });
+
 const app = express();
-const PORT = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.json());
-app.listen(PORT);
-bot.on("message", async (msg) => {
+
+// const client = new Client({
+//   user: 'postgres',
+//   host: 'localhost',
+//   database: 'tgbotfarsh',
+//   password: 'root',
+//   port: 5432, 
+// });
+// client.connect();
+
+bot.on("message", async msg => {
   const chatId = msg.chat.id;
   const text = msg.text;
 
   if (text === "/start") {
     await bot.sendMessage(chatId, "Ниже появится кнопка", {
       reply_markup: {
-        keyboard: [[{ text: "Сделать заказ", web_app: { url: webAppUrl } }]],
+        keyboard: [[{ text: "Сделать заказ", web_app: { url: process.env.WEBAPPURL } }]],
       },
     });
   }
   if (msg?.web_app_data?.data) {
     const data = msg?.web_app_data?.data;
-    const parsedData = parseData(data);
-    console.log('parsedData',parsedData);
-
-    // Пример данных, полученных после парсинга
-    const user = parsedData.user;
-    const products = parsedData.products;
+    const { parsedData, stringOfData, cart} = parseData(data);
+    console.log('parsedData', parsedData);
 
     try {
       // Вставка данных в таблицу orders
@@ -40,32 +47,34 @@ bot.on("message", async (msg) => {
         VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, CURRENT_TIMESTAMP)
         RETURNING id;
       `;
-      const orderValues = [
-        user.name,
-        user.email,
-        user.phone,
-        user.comment,
-        user.adress.city,
-        user.adress.street,
-        user.adress.house,
-        user.adress.building,
-        user.adress.apartment,
-      ];
 
-      const res = await client.query(orderQuery, orderValues);
-      const orderId = res.rows[0].id; // Получаем id созданного заказа
+      // const orderValues = [
+      //   parsedData.name,
+      //   parsedData.email,
+      //   parsedData.phone,
+      //   parsedData.comment,
+      //   parsedData.city,
+      //   parsedData.street,
+      //   parsedData.house,
+      //   parsedData.building,
+      //   parsedData.apartment,
+      // ];
+      const { cart, ...orderValues } = parsedData
 
-      // Вставка данных продуктов в таблицу order_products
-      for (const product of products) {
-        const productQuery = `
-          INSERT INTO order_products (order_id, menu_name, quantity)
-          VALUES ($1, $2, $3);
-        `;
-        const productValues = [orderId, product.menu_name, product.quantity];
-        await client.query(productQuery, productValues);
-      }
+      // const res = await client.query(orderQuery, orderValues);
+      // const orderId = res.rows[0].id; // Получаем id созданного заказа
 
-      await bot.sendMessage(chatId, `Ваш заказ был успешно сохранен!`);
+      // // Вставка данных продуктов в таблицу order_products
+      // for (const product of cart) {
+      //   const productQuery = `
+      //     INSERT INTO order_products (order_id, menu_name, quantity)
+      //     VALUES ($1, $2, $3);
+      //   `;
+      //   const productValues = [orderId, product.menu_name, product.quantity];
+      //   await client.query(productQuery, productValues);
+      // }
+
+      await bot.sendMessage(chatId, `Ваш заказ был успешно сохранен! ${stringOfData}`);
     } catch (error) {
       console.error("Ошибка при сохранении заказа:", error);
       await bot.sendMessage(
